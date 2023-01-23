ce22a9aa260247d16b38642497f1d7fff08892dd
GXR1LL4Z <crisza2001@gmail.com>
2023-01-23 12:15:47 +0100

    origin

--- nauka_1/test.py
+++ nauka_1/test.py
@@ -1,6 +1,6 @@
 import json 
 
-with open('test.json') as czynnosc:
+with open('/home/krzysztof/python_ws/1_quiz/nauka_1/test.json') as czynnosc:
     data = json.load(czynnosc)
 for n in range(10):
     print(data)
